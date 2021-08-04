a24f77fd99ad42f117690ab31d441ddc52400fac
caplincapture <leuping@gmail.com>
2021-08-04 10:00:44 -0700

    handling

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization:
           'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
       },
     });
+
+    console.log(response.data.results);
   }
 
   render() {
