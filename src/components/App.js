313b666dfd55303c914883bfeb9afb8130b94341
caplincapture <leuping@gmail.com>
2021-08-04 10:00:10 -0700

    viewing

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {}
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
+      },
+    });
+  }
 
   render() {
     return (
